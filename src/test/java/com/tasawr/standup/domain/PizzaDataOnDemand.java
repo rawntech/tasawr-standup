89d0442cfeb0fe0ac85de81e1c9d7d079066e895
Nurul Ferdous <hello@dynamicguy.com>
2012-12-24 15:46:11 +0600

    junks removed

--- src/test/java/com/tasawr/standup/domain/PizzaDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package com.tasawr.standup.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Pizza.class)
-public class PizzaDataOnDemand {
-}
--- src/test/java/com/tasawr/standup/domain/PizzaOrderDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package com.tasawr.standup.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = PizzaOrder.class)
-public class PizzaOrderDataOnDemand {
-}
