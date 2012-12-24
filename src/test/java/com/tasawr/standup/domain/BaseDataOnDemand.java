e9fb3321d1a405def5789d79b93430d020205579
Nurul Ferdous <hello@dynamicguy.com>
2012-12-24 14:55:16 +0600

    working version with selenese tests

---
+++ src/test/java/com/tasawr/standup/domain/BaseDataOnDemand.java
@@ -0,0 +1,7 @@
+package com.tasawr.standup.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Base.class)
+public class BaseDataOnDemand {
+}
---
+++ src/test/java/com/tasawr/standup/domain/PizzaOrderDataOnDemand.java
@@ -0,0 +1,7 @@
+package com.tasawr.standup.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = PizzaOrder.class)
+public class PizzaOrderDataOnDemand {
+}
